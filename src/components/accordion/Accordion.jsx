cb34cf640529b7b025821a6a5e6c5d9fa273abee
soumya007GitHub <soumyatripathy2021@gmail.com>
2026-06-19 18:25:01 +0530

    Added multiselect accordion

--- src/components/accordion/Accordion.jsx
+++ src/components/accordion/Accordion.jsx
@@ -5,17 +5,38 @@ import "./style.css";
 
 const Accordion = () => {
     const [selected, setSelected] = useState(null);
+    const [multiSelect, setMultiSelect] = useState(false);
+    const [selectedItems, setSelectedItems] = useState([]);
+
+    const multiSelectHelperAddRemove = (ID) => {
+        if (selectedItems.indexOf(ID) === -1) {
+            setSelectedItems([...selectedItems, ID]);
+        } else {
+            const cpySelectedItems = [...selectedItems];
+            cpySelectedItems.splice(selectedItems.indexOf(ID), 1);
+            setSelectedItems([...cpySelectedItems]);
+        }
+        console.log(selectedItems);
+    }
+
     return (
         <div className='wrapper'>
+            <h3 className='heading'>Accordion</h3>
+            <button onClick={() => setMultiSelect(!multiSelect)} className='btn'>Enable Multiselect</button>
             <div className='accordion'>
                 {
                     accordionData && accordionData.map((item) => {
                         return (
-                            <div className='item' key={item.id} onClick={()=>setSelected(selected == item.id ? null : item.id)}>
+                            <div className='item' key={item.id} onClick={
+                                multiSelect ? ()=>multiSelectHelperAddRemove(item.id)
+                                    : () => setSelected(selected == item.id ? null : item.id)
+                            }>
                                 <div className='title'>
                                     <span>{item.title}</span>
                                 </div>
-                                <div className='body'>{selected == item.id ? item.content : null}</div>
+                                <div className='body'>{
+                                    multiSelect ? selectedItems.indexOf(item.id) != -1 ? item.content : null : selected == item.id ? item.content : null
+                                }</div>
                             </div>
                         )
                     })
